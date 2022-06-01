91d6c5bd67e69aa385af69523a411da8dce6143d
brionnevi <victor.brionne@gmail.com>
2022-06-01 19:57:48 +0200

    PWA configuration and manifest link

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import ButtonAppBar from '../components/navigation/ButtonAppBar'
 import {createTheme, ThemeProvider} from '@mui/material/styles';
 import {CssBaseline} from "@mui/material";
+import Head from 'next/head';
 
 const customTheme = createTheme({
     palette: {
@@ -11,11 +12,17 @@ const customTheme = createTheme({
 
 function MyApp({Component, pageProps}) {
     return (
-        <ThemeProvider theme={customTheme}>
-            <CssBaseline/>
-            <ButtonAppBar/>
-            <Component {...pageProps} />
-        </ThemeProvider>
+        <>
+            <Head>
+                <link rel="manifest" href="/public/manifest.json"/>
+            </Head>
+            <ThemeProvider theme={customTheme}>
+                <CssBaseline/>
+                <ButtonAppBar/>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+
     )
 }
 
